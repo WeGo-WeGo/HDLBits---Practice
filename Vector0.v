4c36ef5c6a48f7cc877bfeb567d575e65f4acc20
WeGo-WeGo <xueyouhe0@gmail.com>
2026-07-05 14:23:02 +0800

    Add top_module to output vector elements

---
+++ Vector0.v
@@ -0,0 +1,7 @@
+module top_module (input wire [2:0] vec, output wire[2:0] outv, output wire o0, o1, o2 );
+    assign o0 =  vec[0];
+    assign o1 = vec[1];
+    assign o2 = vec[2];
+    assign outv = vec;
+    
+endmodule
